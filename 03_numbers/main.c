b8d6655abc9d27e22ef410c79aa868b1af6005dc
Vasily Vyalkov <blackdeskcorp@gmail.com>
2020-11-03 14:43:33 +0000

    Bin numbers

--- 03_numbers/main.c
+++ 03_numbers/main.c
@@ -4,6 +4,10 @@ int main(void) {
     int x = 0;
     while (scanf("%d", &x) == 1) {
         printf("%x\n", x);
+        for (int i = sizeof(x) * 8; i > 0; --i) {
+	    printf("%d", 1 << (i - 1) & x ? 1 : 0);
+  	}
+        printf("\n");
     }
     return 0;
 }

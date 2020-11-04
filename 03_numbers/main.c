96f3564cb799a8eea6e01053f2361ace706cfe1b
Alexander Myltsev <a.myltsev@gmail.com>
2020-11-04 10:19:15 +0000

    Fix formatting.

--- 03_numbers/main.c
+++ 03_numbers/main.c
@@ -5,8 +5,8 @@ int main(void) {
     while (scanf("%d", &x) == 1) {
         printf("%x\n", x);
         for (int i = sizeof(x) * 8; i > 0; --i) {
-	    printf("%d", 1 << (i - 1) & x ? 1 : 0);
-  	}
+            printf("%d", 1 << (i - 1) & x ? 1 : 0);
+        }
         printf("\n");
     }
     return 0;

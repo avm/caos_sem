88dab5096791411783b72ea5836d3d76d87cc98c
Student <student@caos>
2020-11-17 13:50:12 +0000

    Added fopen

--- 04_cat/cat.c
+++ 04_cat/cat.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int main(void) {
-    int str;
-    while ((str = getchar()) != EOF) {
-        putchar(str);
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            FILE *f = fopen(argv[i], "rb");
+            if (!f) {
+                fprintf(stderr, "fopen failed: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            int ch;
+            while ((ch = fgetc(f)) != EOF) {
+                putchar(ch);
+            }
+            fclose(f);
+        }
+    } else {
+        int str;
+        while ((str = getchar()) != EOF) {
+            putchar(str);
+        }
     }
     return 0;
 }

efee8dc45e3a06d740287a00107be343bece5f83
Student <student@caos>
2020-12-01 13:27:48 +0000

    Fix string_init.

--- 07_strings/strings.c
+++ 07_strings/strings.c
@@ -4,11 +4,10 @@
 
 void string_init(struct String* string, const char* initial) {
     size_t len = strlen(initial) + 1;
-    string->data = malloc(sizeof(char) * len);
+    string->data = strdup(initial);
     if (string->data == NULL) {
         abort();
     }
-    strncpy(string->data, initial, len);
     string->length = len;
     string->capacity = len;
 }
@@ -20,7 +19,6 @@ void string_free(struct String* string) {
     string->length = 0;
     string->capacity = 0;
 }
-
 
 
 void string_append(struct String* string, int c) {
